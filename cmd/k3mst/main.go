60bd5ac692f275d68933f7419e4c3e86b250c561
Bozhidar Argirov <bozhdiar.ar@sbtech.com>
2018-10-06 11:42:37 +0300

    Add Basic Diagnostics

--- cmd/k3mst/main.go
+++ cmd/k3mst/main.go
@@ -1,16 +1,36 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
 
-import "net/http"
+	"github.com/bapiens/k3mst/internal/diagnostics"
+	"github.com/gorilla/mux"
+
+	"net/http"
+)
 
 func main() {
-	log.Print("Ko sta")	
+	log.Print("Ko sta")
 
-	err := http.ListenAndServe(":8080", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/", hello)
+
+	go func() {
+		err := http.ListenAndServe(":8080", router)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	diagnostics = diagnostics.NewDiagnostics()
+
+	err := http.ListenAndServe(":8585", diagnostics)
 	if err != nil {
 		log.Fatal(err)
 	}
-		
 }
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, http.StatusText(http.StatusOK))
+}
